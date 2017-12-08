c6492668c40bedce02d8a57ba8b51d89e9e502d6
Tim Wood <timwood@gwu.edu>
2017-12-08 12:15:14 -0800

    CloudLab profile script

---
+++ onvm-tut.py
@@ -0,0 +1,62 @@
+"""A chain of servers running OpenNetVM. Each server has tools such as iperf and nginx.
+
+Instructions:
+Specify the chain length (minimum of 3). To initialize OpenNetVM run:
+```
+  cd /local/onvm/openNetVM/scripts  
+  source setup_cloudlab.sh  
+  ./setup_environment.sh
+```
+
+"""
+
+import geni.portal as portal
+import geni.rspec.pg as rspec
+
+# Create a Request object to start building the RSpec.
+request = portal.context.makeRequestRSpec()
+
+# Describe the parameter(s) this profile script can accept.
+portal.context.defineParameter( "n", "Number of Hosts (minimum 3)", portal.ParameterType.INTEGER, 3 )
+
+# Retrieve the values the user specifies during instantiation.
+params = portal.context.bindParameters()
+
+nodes = []
+cnt = 1
+NUM_NODES = params.n
+
+for n in range(NUM_NODES):
+	node = request.RawPC("node" + str(cnt))
+	node.hardware_type = "c220g1"
+	node.disk_image = 'urn:publicid:IDN+wisc.cloudlab.us+image+gwcloudlab-PG0:onvm_17.11:1'
+	nodes.append(node)
+	cnt = cnt + 1
+
+# Link 1---(n-2)
+for n in range(1,NUM_NODES-2):
+	if1 = nodes[n].addInterface()
+	if2 = nodes[n+1].addInterface()
+	link = request.Link("link" + str(n) + "-" + str(n+1))
+	link.addInterface(if1)
+	link.addInterface(if2)
+
+# Link 0---1
+n = 0
+if1 = nodes[n].addInterface()
+if1.addAddress(rspec.IPv4Address("192.168.1.1", "255.255.255.0"))
+if2 = nodes[n+1].addInterface()
+link = request.Link("link" + str(n) + "-" + str(n+1))
+link.addInterface(if1)
+link.addInterface(if2)
+# Link (n-2)---(n-1)
+n = NUM_NODES-2
+if1 = nodes[n].addInterface()
+if2 = nodes[n+1].addInterface()
+if2.addAddress(rspec.IPv4Address("192.168.1." + str(NUM_NODES), "255.255.255.0"))
+link = request.Link("link" + str(n) + "-" + str(n+1))
+link.addInterface(if1)
+link.addInterface(if2)
+
+# Print the RSpec to the enclosing page.
+portal.context.printRequestRSpec()
